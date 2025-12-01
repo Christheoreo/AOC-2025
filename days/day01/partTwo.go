5f29e04b1ab7084f12d75bb76c683f45504df57c
Chris <chris.finlow@gmail.com>
2025-12-01 08:59:14 +0000

    feat: completed part 2

--- days/day01/partTwo.go
+++ days/day01/partTwo.go
@@ -5,42 +5,100 @@ import (
 	"strings"
 )
 
+type Lock struct {
+	pointer              int
+	AmountOfLandedOnZero int
+}
+
+func NewLock() *Lock {
+	return &Lock{
+		pointer:              50,
+		AmountOfLandedOnZero: 0,
+	}
+}
+
+func (l *Lock) turnLeft() {
+	l.pointer -= 1
+	if l.pointer == 0 {
+		l.AmountOfLandedOnZero += 1
+	}
+	if l.pointer == -1 {
+		l.pointer = 99
+	}
+}
+
+func (l *Lock) turnRight() {
+	l.pointer += 1
+	if l.pointer == 100 {
+		l.AmountOfLandedOnZero += 1
+		l.pointer = 0
+	}
+}
+
+func (l *Lock) TurnLeftX(x int) {
+	for i := 0; i < x; i += 1 {
+		l.turnLeft()
+	}
+}
+
+func (l *Lock) TurnRightX(x int) {
+	for i := 0; i < x; i += 1 {
+		l.turnRight()
+	}
+}
+
 func SolvePartTwo(input string) int {
 	lines := strings.Split(input, "\n")
-
-	answer := 0
-	pointer := 50
+	l := NewLock()
 	for _, line := range lines {
 		direction := line[0]
 		numberS := line[1:]
 		num, _ := strconv.Atoi(numberS)
-		startedAtZero := pointer == 0
+		// startedAtZero := pointer == 0
 		if direction == 'L' {
-			pointer -= num
-			rotated := false
-			for pointer < 0 {
-				rotated = true
-				pointer += 100
-				// only add it we went past 0
-				if !startedAtZero || (startedAtZero && num < -99) {
-					answer += 1
-				}
-			}
-
-			if !rotated && pointer == 0 {
-				answer += 1
-			}
-
+			l.TurnLeftX(num)
 		} else {
 			// R
-			pointer += num
-			for pointer > 99 {
-				pointer -= 100
-				answer += 1
-			}
+			l.TurnRightX(num)
 		}
-
 	}
 
-	return answer
+	return l.AmountOfLandedOnZero
 }
+
+// func SolvePartTwo(input string) int {
+// 	lines := strings.Split(input, "\n")
+
+// 	answer := 0
+// 	pointer := 50
+// 	for _, line := range lines {
+// 		direction := line[0]
+// 		numberS := line[1:]
+// 		num, _ := strconv.Atoi(numberS)
+// 		// startedAtZero := pointer == 0
+// 		if direction == 'L' {
+// 			pointer -= num
+// 			if pointer == 0 {
+// 				answer += 1
+// 				continue
+// 			}
+
+// 			if pointer < -99 {
+// 				rotation := pointer % -100
+// 				answer += rotation
+// 				pointer += (rotation * 100)
+// 			}
+
+// 		} else {
+// 			// R
+// 			pointer += num
+// 			for pointer > 99 {
+// 				pointer -= 100
+// 				answer += 1
+// 			}
+// 		}
+
+// 	}
+
+// 	return answer
+// }
