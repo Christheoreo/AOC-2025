26451c383596c5942f01726df82f215836eaac7d
17OChris <chris@17oranges.com>
2025-12-02 07:30:17 +0000

    feat: part01 completed

---
+++ days/day02/partOne.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+func SolvePartOne(input string) int {
+	answer := 0
+	ranges := strings.Split(input, ",")
+
+	m := make(map[int]bool)
+
+	for _, r := range ranges {
+		var start, end int
+		parts := strings.Split(r, "-")
+		start, _ = strconv.Atoi(parts[0])
+		end, _ = strconv.Atoi(parts[1])
+
+		for i := start; i <= end; i += 1 {
+			if val, exists := m[i]; exists && val {
+				answer += i
+				continue
+			}
+			if isAPattern(i) {
+				m[i] = true
+				answer += i
+				continue
+			}
+			m[i] = false
+		}
+	}
+
+	return answer
+}
+
+func isAPattern(number int) bool {
+	s := strconv.Itoa(number)
+
+	if len(s)%2 != 0 {
+		return false
+	}
+
+	halfway := len(s) / 2
+	return s[0:halfway] == s[halfway:]
+}
---
+++ days/day02/partTwo.go
@@ -0,0 +1,6 @@
+package main
+
+// This is a BAD solution, but a working one regardless!
+func SolvePartTwo(input string) int {
+	return 1
+}
