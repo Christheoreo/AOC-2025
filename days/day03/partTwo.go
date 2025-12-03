f393de01483c984416d088f2f8422d91d2f31ae1
Chris <chris.finlow@gmail.com>
2025-12-03 09:16:08 +0000

    docs: added comments

--- days/day03/partTwo.go
+++ days/day03/partTwo.go
@@ -10,12 +10,15 @@ func SolvePartTwo(input string) int {
 	answer := 0
 	lines := strings.Split(input, "\n")
 	lineLength := len(lines[0])
+
+	// Make a map of where each index maps to the earliest index of the selectedBatteries
 	m := make(map[int]int)
 	x := 11
 	for i := lineLength - 1; i >= lineLength-12; i -= 1 {
 		m[i] = x
 		x -= 1
 	}
+
 	for _, line := range lines {
 		var selectedBatteries [12]int
 		for i, c := range line {
@@ -24,10 +27,12 @@ func SolvePartTwo(input string) int {
 			earliestIndex, ok := m[i]
 
 			for ii := 0; ii < len(selectedBatteries); ii += 1 {
+				// if int isnt in map, we know we can start from 0, else we want to skip to the earliest index
 				if ok && ii < earliestIndex {
 					continue
 				}
 				if num > selectedBatteries[ii] {
+					// update the number and then reset everything after it
 					selectedBatteries[ii] = num
 					for iii := ii + 1; iii < len(selectedBatteries); iii += 1 {
 						selectedBatteries[iii] = 0
@@ -36,8 +41,8 @@ func SolvePartTwo(input string) int {
 				}
 			}
 		}
+		// this feels horrinle!
 		num, _ := strconv.Atoi(fmt.Sprintf("%d%d%d%d%d%d%d%d%d%d%d%d", selectedBatteries[0], selectedBatteries[1], selectedBatteries[2], selectedBatteries[3], selectedBatteries[4], selectedBatteries[5], selectedBatteries[6], selectedBatteries[7], selectedBatteries[8], selectedBatteries[9], selectedBatteries[10], selectedBatteries[11]))
-		fmt.Printf("Adding %d to total %d\n", num, answer)
 		answer += num
 	}
 	return answer
