66ef0584de0bda4e14760070a90ec947f9e200b0
Chris <chris.finlow@gmail.com>
2025-12-04 08:48:56 +0000

    refactor: cleaned code

--- days/day04/partTwo.go
+++ days/day04/partTwo.go
@@ -2,74 +2,6 @@ package main
 
 import "strings"
 
-// type Grid2 struct {
-// 	rows [][]byte
-// }
-
-// func NewGrid2(rows [][]byte) *Grid2 {
-// 	return &Grid2{
-// 		rows: rows,
-// 	}
-// }
-
-// func (g *Grid2) CountRollsInAdjacentPositions(row, col int) int {
-// 	// 8 Adject posisitions
-// 	count := 0
-// 	// can we go up?
-// 	canLookLeft := col > 0
-// 	canLookRight := col < len(g.rows[0])-1
-// 	canLookUp := row > 0
-// 	canLookDown := row < len(g.rows)-1
-// 	if canLookUp {
-// 		if canLookLeft {
-// 			if g.rows[row-1][col-1] == '@' {
-// 				count += 1
-// 			}
-// 		}
-// 		// top middle
-// 		if g.rows[row-1][col] == '@' {
-// 			count += 1
-// 		}
-// 		// top right
-// 		if canLookRight {
-// 			if g.rows[row-1][col+1] == '@' {
-// 				count += 1
-// 			}
-// 		}
-// 	}
-
-// 	if canLookLeft {
-// 		if g.rows[row][col-1] == '@' {
-// 			count += 1
-// 		}
-// 	}
-
-// 	if canLookRight {
-// 		if g.rows[row][col+1] == '@' {
-// 			count += 1
-// 		}
-// 	}
-
-// 	if canLookDown {
-// 		if canLookLeft {
-// 			if g.rows[row+1][col-1] == '@' {
-// 				count += 1
-// 			}
-// 		}
-// 		// bottom middle
-// 		if g.rows[row+1][col] == '@' {
-// 			count += 1
-// 		}
-// 		// bottom right
-// 		if canLookRight {
-// 			if g.rows[row+1][col+1] == '@' {
-// 				count += 1
-// 			}
-// 		}
-// 	}
-// 	return count
-// }
-
 func SolvePartTwo(input string) int {
 	answer := 0
 
@@ -102,12 +34,13 @@ func SolvePartTwo(input string) int {
 			}
 		}
 
+		// We can't remove anymore!
 		if len(positionsToRemove) == 0 {
 			break
 		}
 
-		for _, x := range positionsToRemove {
-			rows[x[0]][x[1]] = '.'
+		for _, posToRemove := range positionsToRemove {
+			rows[posToRemove[0]][posToRemove[1]] = '.'
 		}
 
 		g = NewGrid(rows)
