ec7fd2c819e3427eb5a82199b4595e3cf8a5a90a
Chris <chris.finlow@gmail.com>
2025-12-05 14:25:20 +0000

    feat: not solvevd yet

--- days/day05/partTwo.go
+++ days/day05/partTwo.go
@@ -8,11 +8,8 @@ import (
 func SolvePartTwo(input string) int {
 	answer := 0
 	ranges := make([][2]int, 0)
-	Overlappingranges := make([][2]int, 0)
 
 	lines := strings.Split(input, "\n")
-	// key is the min, value is the max
-	// m := make(map[int]int)
 	for _, line := range lines {
 		if line == "" {
 			break
@@ -25,73 +22,18 @@ func SolvePartTwo(input string) int {
 		ranges = append(ranges, [2]int{lower, higher})
 	}
 
-	// TODO-
-	// dont loop through i, just frorever look at first rnage, deal with it and move it
-
-	for len(ranges) > 0 {
-
-		r := ranges[0]
-
-		adjusted := false
-		for ii := 0; ii < len(Overlappingranges); ii += 1 {
-			rr := Overlappingranges[ii]
-			adjusted = false
-
-			// can we just ignore it?
-			if r[0] >= rr[0] && r[1] <= rr[1] {
-				// fitd within it, so lets ignore it
-				break
-			}
-
-			if r[0] < rr[0] {
-				// weve extended the bottom range
-				if r[1] < rr[1] && r[1] > rr[0] {
-					Overlappingranges[ii][0] = r[0]
-					adjusted = true
-				}
-
-				if r[1] > rr[1] {
-					Overlappingranges[ii][1] = r[1]
-					adjusted = true
-				}
-			}
-
-			if r[0] > rr[0] && r[0] < rr[1] {
-
-				// lets see if we can extend range
-				if r[1] > rr[1] {
-					Overlappingranges[ii][1] = r[1]
-					adjusted = true
-				}
-			}
-
-			if adjusted {
-				break
-			}
-		}
-
-		ranges = ranges[1:]
-
-		if !adjusted {
-			Overlappingranges = append(Overlappingranges, r)
-		}
-
-		// remove r from ranges now? and for any time we adjust it above?
-
-	}
-
 	for {
 		madeAChange := false
-		for i := 0; i < len(Overlappingranges); i += 1 {
-			r := Overlappingranges[i]
+		for i := 0; i < len(ranges); i += 1 {
+			r := ranges[i]
 			if r[0] == 0 && r[1] == 0 {
 				continue
 			}
-			for k := 0; k < len(Overlappingranges); k += 1 {
+			for k := 0; k < len(ranges); k += 1 {
 				if i == k {
 					continue
 				}
-				rr := Overlappingranges[k]
+				rr := ranges[k]
 				if rr[0] == 0 && rr[1] == 0 {
 					continue
 				}
@@ -99,27 +41,27 @@ func SolvePartTwo(input string) int {
 
 				// check if rr can go inside r
 				if rr[0] >= r[0] && rr[1] <= r[1] {
-					Overlappingranges[k] = [2]int{0, 0}
+					ranges[k] = [2]int{0, 0}
 					continue
 				}
 
 				// does the lower boundary need changing?
 				if rr[0] < r[0] && rr[1] >= r[0] {
-					Overlappingranges[i][0] = Overlappingranges[k][0]
-					// Overlappingranges[k] = [2]int{0, 0}
+					ranges[i][0] = ranges[k][0]
+					// ranges[k] = [2]int{0, 0}
 					madeAChange = true
 					localChange = true
 				}
 
 				// does the upper boundary need changing?
 				if rr[1] > r[1] && rr[0] <= r[1] {
-					Overlappingranges[i][1] = Overlappingranges[k][1]
+					ranges[i][1] = ranges[k][1]
 					madeAChange = true
 					localChange = true
 				}
 
 				if localChange {
-					Overlappingranges[k] = [2]int{0, 0}
+					ranges[k] = [2]int{0, 0}
 				}
 			}
 		}
@@ -130,12 +72,14 @@ func SolvePartTwo(input string) int {
 
 	}
 
-	for _, r := range Overlappingranges {
+	for _, r := range ranges {
 		if r[0] == 0 && r[1] == 0 {
 			continue
 		}
 
-		answer += (r[1] - r[0]) + 1
+		diff := (r[1] - r[0]) + 1
+
+		answer += diff
 	}
 
 	return answer
