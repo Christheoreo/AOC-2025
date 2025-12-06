6a603e90f5b4c3a670a74c05029e2aca293527dc
Chris <chris.finlow@gmail.com>
2025-12-06 07:47:45 +0000

    feat: prepared day06

--- days/day06/main.go
+++ days/day06/main.go
@@ -13,7 +13,7 @@ var input string
 
 func main() {
 	partOneAnswer := SolvePartOne(input)
-	partTwoAnswer := SolvePartTwo(input)
+	// partTwoAnswer := SolvePartTwo(input)
 	fmt.Printf("Day 1 part 1 = %v\n", partOneAnswer)
-	fmt.Printf("Day 1 part 2 = %v\n", partTwoAnswer)
+	// fmt.Printf("Day 1 part 2 = %v\n", partTwoAnswer)
 }
